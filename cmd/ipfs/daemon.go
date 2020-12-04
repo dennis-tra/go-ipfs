21e1bb5d5de1e1f09bc24c18ce2382a110284462
Petar Maymounkov <petarm@gmail.com>
2020-12-04 07:55:40 -0800

    logging

--- cmd/ipfs/daemon.go
+++ cmd/ipfs/daemon.go
@@ -543,6 +543,7 @@ func pinMFSOnChange(cctx *oldcmds.Context, node *core.IpfsNode) (<-chan error, e
 			if lastRootCid == rootCid {
 				continue
 			}
+			log.Infof("For pinning purposes, MFS root CID has changed")
 			lastRootCid = rootCid
 
 			// pin on all remote services in parallel to prevent DoS attacks
@@ -552,6 +553,7 @@ func pinMFSOnChange(cctx *oldcmds.Context, node *core.IpfsNode) (<-chan error, e
 				if svcConfig.Policies.PinMFS == nil || !*svcConfig.Policies.PinMFS {
 					continue
 				}
+				log.Infof("Pinning MFS to %s", svcName)
 				wg.Add(1)
 				go func() {
 					defer wg.Done()
